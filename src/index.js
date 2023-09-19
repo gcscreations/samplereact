ddc3aefb2289e6d3af8f2b6abb979fee32e5d28f
gcscreations <gcscreations.ar.game@gmail.com>
2023-09-19 18:09:33 +0530

    COMMIT SECOND

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import {Controller} from 'mind-ar/dist/mindar-image.prod.js';
+//console.log("MindARThree", Controller);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
